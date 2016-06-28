62645b03e962ad2a7dea94c7cfa7540aae947ee5
peryaudo <peryaudo@gmail.com>
2016-06-28 15:47:38 +0900

    Fix fit.py to work with multi factors

--- scripts/fit.py
+++ scripts/fit.py
@@ -1,45 +1,58 @@
+import subprocess
 import pandas as pd
 import numpy as np
 from sklearn import (svm, linear_model)
 from sklearn.metrics import mean_squared_error
 from sklearn.cross_validation import train_test_split
 import matplotlib.pyplot as plt
+from StringIO import StringIO
 
-fns = ['factors.csv', 'factors_max_edge.csv', 'factors_max_min.csv', 'factors_max_min_edge.csv', 'factors_split_average.csv', 'factors_split_average_max_min_edge.csv', 'gengame_factors_split_average.csv', 'extended_factors_split_average.csv']
+p = subprocess.Popen("./trax --dump_factors", cwd="..", shell=True,
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
+stdout_data, stderr_data = p.communicate()
 
-for filename in fns:
-    print "%s:" % filename
-    data = pd.read_csv(filename)
-    X = data[['endpoint_factor', 'edge_factor']]
-    # X = data[['edge_factor']]
-    # X = data[['endpoint_factor']]
-    y = data['winner']
+data = pd.read_csv(StringIO(stdout_data))
+factors = [
+    'leaf_average','longest_line','edge_color',
+    'endpoint_factor','sum_edge_factor','max_edge_factor',
+    'endpoint_factor_max_min','sum_edge_factor_max_min',
+    'max_edge_factor_max_min',
+    'endpoint_factor_average','sum_edge_factor_average',
+    'max_edge_factor_average']
 
-    match = 0
-    nomatch = 0
-    orig = []
-    pred = []
-    for _ in xrange(0, 30):
-        X_train, X_test, y_train, y_test = train_test_split(X, y)
+# factors = ['endpoint_factor','sum_edge_factor']
 
-        model = linear_model.LinearRegression()
-        # model = svm.SVR(kernel='rbf')
-        model.fit(X_train, y_train)
+print "Correlation to all factors:"
+for factor in factors:
+    print "  %s: %f" % (factor,
+                        np.corrcoef(data['winner'], data[factor])[0][1])
+X = data[factors]
 
-        print model.coef_
+# X = data[['edge_factor']]
+# X = data[['endpoint_factor']]
+y = data['winner']
 
-        origg = list(y_test)
-        predd = list(model.predict(X_test))
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-        for (o, p) in zip(origg, predd):
-            if o * p > 0:
-                match += 1
-            else:
-                nomatch += 1
-        orig.extend(origg)
-        pred.extend(predd)
+model = linear_model.LinearRegression()
+# model = svm.SVR(kernel='rbf')
+model.fit(X_train, y_train)
 
-    print "mse: %f" % mean_squared_error(orig, pred)
-    print "sign accuracy: %f (%d/%d)" % (float(match) / (match + nomatch), match, match + nomatch)
-    print "corr: %f " % np.corrcoef(orig, pred)[0][1]
-    print ""
+print model.coef_
+
+orig = list(y_test)
+pred = list(model.predict(X_test))
+
+match = 0
+nomatch = 0
+for (o, p) in zip(orig, pred):
+    if o * p > 0:
+        match += 1
+    else:
+        nomatch += 1
+
+print "mse: %f" % mean_squared_error(orig, pred)
+print "sign accuracy: %f (%d/%d)" % (float(match) / (match + nomatch),
+                                     match, match + nomatch)
+print "corr: %f " % np.corrcoef(orig, pred)[0][1]
